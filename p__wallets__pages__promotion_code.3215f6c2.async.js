0638b4172d7ec501684867b927e74c91478ab211
ccpWebserver <98396071+ccpWebserver@users.noreply.github.com>
2022-01-29 10:29:08 +0800

    Add files via upload

---
+++ p__wallets__pages__promotion_code.3215f6c2.async.js
@@ -0,0 +1,17 @@
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[958],{60069:function(u,_,t){"use strict";t.r(_);var s=t(20310),r=t(53776),d=t(47416),a=t(85893),i,o=d.ZP.div(i||(i=(0,s.Z)([`
+
+  img {
+    display: block;
+    width: 100%;
+
+  }
+
+  @media (max-width: 576px) {
+    transform: rotate(90deg);
+
+    img{
+      width: 86vh;
+      height: 100vw;
+    }
+  }
+`])));_.default=function(){var n,l=(0,r.t)("@@initialState"),e=l.initialState,m=l.setInitialState;return(0,a.jsx)(o,{children:(0,a.jsx)("img",{src:e==null||(n=e.currentUser)===null||n===void 0?void 0:n.referee_img_url,alt:""})})}}}]);
